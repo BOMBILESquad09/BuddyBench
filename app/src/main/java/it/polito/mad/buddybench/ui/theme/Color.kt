2cd5b5eab37cbf9c508b3840b8893980eb07d7ce
Lorenzo Pantano <l.pantano97@gmail.com>
2023-03-21 19:06:14 +0100

    feat: added skill level colors in Color.kt

--- app/src/main/java/it/polito/mad/buddybench/ui/theme/Color.kt
+++ app/src/main/java/it/polito/mad/buddybench/ui/theme/Color.kt
@@ -8,5 +8,12 @@ val DarkBlue = Color(0xFF1B1B6C)
 val Yellow = Color(0xFFF3C119)
 val LightBlue = Color(0xFF84D0F2)
 
+// ** Skill level colors
+val NewbieGreen = Color(0xFF00BCB1)
+val AmateurGreen = Color(0xFF00BC62)
+val SkilledOrange = Color(0xFFDF7A1D)
+val ProfessionalRed = Color(0xFFBC0022)
+val WorldClassBlack = Color(0xFF2D2D2D)
+
 val LightBlueInput = Color(0x4084D0F2)
 val DarkBlueCaption = Color(0xBF1B1B6C)
