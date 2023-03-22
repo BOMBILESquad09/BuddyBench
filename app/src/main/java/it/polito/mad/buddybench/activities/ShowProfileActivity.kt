7ad9006f767cb07b2607a00d03eaa0af7d6dfc28
Lorenzo Pantano <l.pantano97@gmail.com>
2023-03-22 23:21:39 +0100

    feat: added icons, themes, colors in xml

---
+++ app/src/main/java/it/polito/mad/buddybench/activities/ShowProfileActivity.kt
@@ -0,0 +1,12 @@
+package it.polito.mad.buddybench.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import it.polito.mad.buddybench.R
+
+class ShowProfileActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_show_profile)
+    }
+}
--- app/src/main/java/it/polito/mad/buddybench/ui/theme/Spacing.kt
+++
@@ -1,9 +0,0 @@
-package it.polito.mad.buddybench.ui.theme
-
-import androidx.compose.ui.unit.dp
-
-val PADDING_XS = 4.dp
-val PADDING_SM = 8.dp
-val PADDING_MD = 16.dp
-val PADDING_LG = 24.dp
-val PADDING_XL = 32.dp
