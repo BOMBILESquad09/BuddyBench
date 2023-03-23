2dcb5c1459b41dc43c6b114559336aa76919b44f
victor3099 <s301607@studenti.polito.it>
2023-03-23 22:49:05 +0100

    added menu

--- app/src/main/java/it/polito/mad/buddybench/activities/ShowProfileActivity.kt
+++ app/src/main/java/it/polito/mad/buddybench/activities/ShowProfileActivity.kt
@@ -2,11 +2,36 @@ package it.polito.mad.buddybench.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import android.view.Window
+import androidx.appcompat.widget.Toolbar
 import it.polito.mad.buddybench.R
 
 class ShowProfileActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_show_profile)
+        val toolbar = findViewById<Toolbar>(R.id.toolbar);
+        setSupportActionBar(toolbar)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.menu_profile, menu)
+        println("Menu created")
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle item selection
+        return when (item.itemId) {
+            R.id.edit -> {
+                println("Hello")
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
