b01c5642a5431ca2040bc3b871ff26adc8822bc6
Jaco <jacodecristo1999@gmail.com>
2023-05-23 00:41:03 +0200

    feat: animation on friend request accepted or rejected

--- app/src/main/java/it/polito/mad/buddybench/persistence/firebaseRepositories/CourtRepository.kt
+++ app/src/main/java/it/polito/mad/buddybench/persistence/firebaseRepositories/CourtRepository.kt
@@ -22,10 +22,10 @@ import javax.security.auth.callback.Callback
 class CourtRepository {
     val db = FirebaseFirestore.getInstance()
 
-    fun addTimetable(timeTableDTO: CourtTimeTableDTO){
+    fun addTimetable(timeTableDTO: CourtTimeTableDTO) {
         val name = timeTableDTO.court.name.replace(" ", "_") + "_" + timeTableDTO.court.sport
         val timetable: HashMap<String, HashMap<String, Int>> = HashMap()
-        for (t in timeTableDTO.timeTable){
+        for (t in timeTableDTO.timeTable) {
             val hash: HashMap<String, Int> = HashMap()
             hash["openingTime"] = t.value.first.hour
             hash["closingTime"] = t.value.second.hour
@@ -39,8 +39,8 @@ class CourtRepository {
             }
     }
 
-    suspend fun getCourt(name:String, sport: String, callback: (CourtDTO) -> Unit){
-        withContext(Dispatchers.IO){
+    suspend fun getCourt(name: String, sport: String, callback: (CourtDTO) -> Unit) {
+        withContext(Dispatchers.IO) {
             val courtName = name.replace(" ", "_") + "_" + sport
             val res = db.collection("courts")
                 .document(courtName)
@@ -54,83 +54,102 @@ class CourtRepository {
     fun getCourtTimeTable(name: String, sport: Sports, callback: (CourtTimeTableDTO) -> Unit) {
         val courtName = name.replace(" ", "_") + "_" + sport.name
         db.collection("courts").document(courtName).get()
-            .addOnSuccessListener{
+            .addOnSuccessListener {
                 val courtDTO = it.toObject(CourtDTO::class.java)!!
                 val timetable: HashMap<DayOfWeek, Pair<LocalTime, LocalTime>> = HashMap()
-                for (s in it.data!!["timetable"] as HashMap<String, HashMap<String, Long>>){
+                for (s in it.data!!["timetable"] as HashMap<String, HashMap<String, Long>>) {
                     val openingTime = s.value["openingTime"]!!
                     val closingTime = s.value["closingTime"]!!
-                    timetable.put(DayOfWeek.valueOf(s.key), Pair(LocalTime.of(openingTime.toInt(),0), LocalTime.of(closingTime.toInt(), 0)))
+                    timetable.put(
+                        DayOfWeek.valueOf(s.key),
+                        Pair(
+                            LocalTime.of(openingTime.toInt(), 0),
+                            LocalTime.of(closingTime.toInt(), 0)
+                        )
+                    )
                 }
-                if(courtDTO.facilities == null)
+                if (courtDTO.facilities == null)
                     courtDTO.facilities = listOf()
-                val courtTimeTableDTO = CourtTimeTableDTO(courtDTO, timetable )
+                val courtTimeTableDTO = CourtTimeTableDTO(courtDTO, timetable)
                 callback(courtTimeTableDTO)
-        }
+            }
 
     }
 
 
-    fun getCourtsByDay(sport: Sports, dayOfWeek: LocalDate, onSuccess: (List<CourtDTO>) -> Unit ) {
-        fun filterByMaxReservation(courts: List<CourtDTO>, onSuccess: (List<CourtDTO>) -> Unit){
+    fun getCourtsByDay(sport: Sports, dayOfWeek: LocalDate, onSuccess: (List<CourtDTO>) -> Unit) {
+        fun filterByMaxReservation(courts: List<CourtDTO>, onSuccess: (List<CourtDTO>) -> Unit) {
             var finalCourts = courts
-            if(dayOfWeek == LocalDate.now() && courts.isNotEmpty()){
+            if (dayOfWeek == LocalDate.now() && courts.isNotEmpty()) {
                 db
                     .collection("reservations")
                     .whereIn("court", courts.map { db.document("courts/${it.getId()}") })
                     .whereEqualTo("date", LocalDate.now().toString()).get()
                     .addOnSuccessListener {
                         val hm: HashMap<String, Long> = HashMap()
+                        for (r in it) {
+                            val courtId = (r.data["court"] as DocumentReference).id
+                            hm[courtId] = maxOf(hm[courtId] ?: 0)
+                        }
                         for (r in it){
-                            val endTime = r.data["endTime"] as Long
-                            val courtId = (r.data["court"] as String)
-                            hm[courtId] = maxOf(hm[courtId] ?: 0, endTime)
+                            val courtId = (r.data["court"] as DocumentReference).id
+                            val closingTime = courts.find { c -> c.getId() == courtId }!!.timetable.get(dayOfWeek.dayOfWeek.name)?.closingTime ?: 24
+
+                            finalCourts = finalCourts.filter {c->
+                                if (c.getId() == courtId) {
+                                    (LocalTime.now().hour < (closingTime - 1) && (hm[courtId] ?: 0) <  LocalTime.now().hour)
+                                } else {
+                                    true
+                                }
+                            }
                         }
-                        finalCourts = finalCourts.filter { c ->
-                            val courtId = c.getId()
-                            (hm[courtId] ?: 0) > LocalTime.now().hour
-                        }
+
                         onSuccess(finalCourts)
+                        return@addOnSuccessListener
                     }
             }
+
             onSuccess(finalCourts)
         }
 
 
         db.collection("courts")
             .whereEqualTo("sport", sport.name)
-            .whereGreaterThan("timetable.${dayOfWeek.dayOfWeek.name}.openingTime", 0)
-            .get().addOnSuccessListener {
-                courtsDoc ->
+            .whereGreaterThan("timetable.${dayOfWeek.dayOfWeek.name}.openingTime", -1)
+            .get().addOnSuccessListener { courtsDoc ->
                 db.collection("unavailable_courts")
                     .document(dayOfWeek.toString())
                     .get()
                     .addOnSuccessListener { it ->
                         var courts = courtsDoc.toObjects(CourtDTO::class.java)
-                        println(LocalDate.now())
-                        println(LocalTime.now().hour)
-                        println(dayOfWeek)
-                        println("-------------------------")
-                        if(dayOfWeek == LocalDate.now()){
+                        if (dayOfWeek == LocalDate.now()) {
                             courts = courts.filter {
                                 (it.timetable[dayOfWeek.dayOfWeek.name]!!.closingTime - 1) > LocalTime.now().hour
                             }
                         }
-                        if (it.data != null){
-                            val courtsToDelete = (it.data!!["courts"] as List<DocumentReference>).map {
-                                val tokens = it.id.split("_").toMutableList()
-                                tokens.removeLast()
-                                tokens.joinToString(" ")
-                            }
-                            filterByMaxReservation(courts.filter { c -> !courtsToDelete.contains(c.name) }, onSuccess)
-                        } else{
+                        if (it.data != null) {
+                            val courtsToDelete =
+                                (it.data!!["courts"] as List<DocumentReference>).map {
+                                    val tokens = it.id.split("_").toMutableList()
+                                    tokens.removeLast()
+                                    tokens.joinToString(" ")
+                                }
+                            filterByMaxReservation(
+                                courts.filter { c -> !courtsToDelete.contains(c.name) },
+                                onSuccess
+                            )
+                        } else {
                             filterByMaxReservation(courts, onSuccess)
                         }
                     }
             }
     }
 
-    fun getTimeSlotsOccupiedForCourtAndDate(court: CourtDTO, date: LocalDate, callback: (List<LocalTime>) -> Unit) {
+    fun getTimeSlotsOccupiedForCourtAndDate(
+        court: CourtDTO,
+        date: LocalDate,
+        callback: (List<LocalTime>) -> Unit
+    ) {
         val courtName = court.getId()
         db
             .collection("reservations")
@@ -139,37 +158,54 @@ class CourtRepository {
             .get()
             .addOnSuccessListener {
                 val timeslots = mutableListOf<ReservationDTO>()
-                for( res in it){
+                for (res in it) {
                     val reservationDTO = ReservationDTO()
-                    reservationDTO.startTime = LocalTime.of((res.data["startTime"] as Long).toInt(), 0 )
+                    reservationDTO.startTime =
+                        LocalTime.of((res.data["startTime"] as Long).toInt(), 0)
                     reservationDTO.endTime = LocalTime.of((res.data["endTime"] as Long).toInt(), 0)
                     timeslots.add(reservationDTO)
 
                 }
-                callback(timeslots.map { Utils.getTimeSlots(it.startTime, it.endTime) }.flatten().toList())
+                callback(
+                    timeslots.map { Utils.getTimeSlots(it.startTime, it.endTime) }.flatten()
+                        .toList()
+                )
 
             }
 
     }
 
-    fun checkIfPlayed(courtName: String, sport: String, userEmail: String, callback: (Boolean) -> Unit) {
+    fun checkIfPlayed(
+        courtName: String,
+        sport: String,
+        userEmail: String,
+        callback: (Boolean) -> Unit
+    ) {
         val courtDocName = courtName.replace(" ", "_") + "_" + sport
         db.collection("reservations")
             .whereEqualTo("user", db.document("users/$userEmail"))
             .whereEqualTo("court", db.document("courts/$courtDocName"))
             .get()
             .addOnSuccessListener {
-               
+
                 if (it.size() == 0) {
                     callback(false)
                     return@addOnSuccessListener
                 }
-                val dates = it.map { d -> Pair(LocalDate.parse(d.data["date"] as String, DateTimeFormatter.ISO_LOCAL_DATE), (d.data["endTime"] as Long).toInt()) }
+                val dates = it.map { d ->
+                    Pair(
+                        LocalDate.parse(
+                            d.data["date"] as String,
+                            DateTimeFormatter.ISO_LOCAL_DATE
+                        ), (d.data["endTime"] as Long).toInt()
+                    )
+                }
                 val minDate = dates.minOfOrNull { d -> d.first }!!
-                val minEndTime = dates.filter { t -> t.first == minDate }.minOfOrNull { t -> t.second }!!
+                val minEndTime =
+                    dates.filter { t -> t.first == minDate }.minOfOrNull { t -> t.second }!!
 
-                if(minDate == LocalDate.now()){
-                    callback(minEndTime <= LocalTime.now().hour )
+                if (minDate == LocalDate.now()) {
+                    callback(minEndTime <= LocalTime.now().hour)
                 } else {
                     callback(minDate < LocalDate.now())
                 }
